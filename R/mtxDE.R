c056c33586aea35de00ba63e0aa7c14d305fe3e9
John Sterrett <sterrettJD@protonmail.com>
2024-01-02 15:22:13 -0700

    convert to accept formula

--- R/mtxDE.R
+++ R/mtxDE.R
@@ -2,9 +2,9 @@ library(gamlss)
 
 # runs a zero-inflated beta distribution GAM using gamlss
 # returns the model
-run_model <- function(data){
-    mod<-gamlss(outcome~1, sigma.formula=~1, nu.formula=~1,
-                data=data,
-                family=BEZI)
+run_mtxDE <- function(formula, data){
+    mod <- gamlss::gamlss(as.formula(formula),
+                        data=data,
+                        family=gamlss.dist::BEZI)
     return(mod)
 }
