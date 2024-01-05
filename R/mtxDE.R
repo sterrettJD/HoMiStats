ebaf04fdeeb5f2533f7c43f71399683dcd6573fc
John Sterrett <sterrettJD@protonmail.com>
2024-01-05 15:26:15 -0700

    p adjustment

--- R/mtxDE.R
+++ R/mtxDE.R
@@ -18,18 +18,18 @@ run_single_beta_regression <- function(formula, data){
     return(mod)
 }
 
-
 #' Run differential expression analysis for metatranscriptomics data
 #' @description Regresses each feature using the formula provided and returns the statistical summary for each feature
 #' @param formula the right hand side of the regression formula to be used for each feature
 #' @param feature.table A dataframe, where rows are samples, and columns are genes/features. Row names should be sample IDs.
 #' @param metadata The metadata dataframe, where rows are samples and columns are features
 #' @param sampleID The name of the column in metadata with sample IDs, which should be used to merge metadata with the feature table's rownames
+#' @param padj p value adustment method. Options can be checked using 'p.adjust.methods'
 #' @return a dataframe with differential expression results
 #' @export
 #' @importFrom broom.mixed tidy
 #'
-run_mtxDE <- function(formula, feature.table, metadata, sampleID){
+run_mtxDE <- function(formula, feature.table, metadata, sampleID, padj){
     # merge the feature table and metadata based on the rownames
     formula <- paste0(" ~ ", formula)
     metadata.vars <- c(all.vars(as.formula(formula)), sampleID)
@@ -49,5 +49,6 @@ run_mtxDE <- function(formula, feature.table, metadata, sampleID){
         mod.sum$feature <- col
         mod.summaries <- rbind(mod.summaries, mod.sum)
     }
+    mod.summaries$q <- p.adjust(mod.summaries$p.value, method=padj)
     return(mod.summaries)
 }
