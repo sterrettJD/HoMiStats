d35a7a68ef5cb5e052c1005baa2a3b8071c0ff67
John Sterrett <sterrettJD@protonmail.com>
2024-01-10 09:17:20 -0700

    test arcsinh

--- tests/testthat/test-transformers.R
+++ tests/testthat/test-transformers.R
@@ -6,4 +6,21 @@ test_that("check_proportional", {
                                   "The following sample does not sum to 1: 3"))
 })
 
+test_that("arcsinh works",{
+    vec <- seq(from=0, to=1, by=0.2)
+    exp.scale.1 <- c(0, 0.198, 0.390, 0.568, 0.732, 0.881)
+    exp.scale.100 <- c(0, 3.689, 4.382, 4.787, 5.075, 5.298)
+    exp.scale.100.norm <- c(0, 0.696, 0.827, 0.903, 0.957, 1)
+
+    expect_equal(arcsinh(vec, scaling.factor=1, norm.by.scaling=F),
+                 exp.scale.1,
+                 tolerance=1e-3)
+    expect_equal(arcsinh(vec, scaling.factor=100, norm.by.scaling=F),
+                 exp.scale.100,
+                 tolerance=1e-3)
+    expect_equal(arcsinh(vec, scaling.factor=100, norm.by.scaling=T),
+                 exp.scale.100.norm,
+                 tolerance=1e-3)
+})
+
 
