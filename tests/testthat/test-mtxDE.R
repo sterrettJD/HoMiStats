ba7e212f825a06165d296c71b93601df8b4d1d19
John Sterrett <sterrettJD@protonmail.com>
2024-01-26 12:20:45 -0700

    add tidying test

---
+++ tests/testthat/test-mtxDE.R
@@ -0,0 +1,28 @@
+test_that("tidy zibr", {
+    dirty.mod <- list(
+        logistic_est_table=data.frame(Estimate=c(0, 0),
+                                      Pvalue=c(1, 1),
+                                      row.names=c("intersept", "phenotype")),
+        logistic_s1_est=1,
+        beta_est_table=data.frame(Estimate=c(0, 0),
+                                  Pvalue=c(1, 1),
+                                  row.names=c("intersept", "phenotype")),
+        beta_s2_est=1,
+        beta_v_est=1,
+        loglikelihood=500,
+        joint_p=c(phenotype=1, depth=1)
+    )
+
+    expected.clean.mod <- data.frame(
+        parameter=c("logistic", "logistic",
+                    "beta", "beta"),
+        term=c("intersept", "phenotype",
+               "intersept", "phenotype"),
+        estimate=c(0, 0, 0, 0),
+        p.value=c(1, 1, 1, 1),
+        joint.p <- c(NA, 1, NA, 1)
+    )
+
+    clean.mod <- tidy_zibr_results(dirty.mod)
+    expect_equal(clean.mod, expected.clean.mod)
+})
