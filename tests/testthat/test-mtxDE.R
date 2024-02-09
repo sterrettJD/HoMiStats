8f47bfd9752c9d6dcf5f5254f3a32934d7aedde6
John Sterrett <sterrettJD@protonmail.com>
2024-02-09 10:45:09 -0700

    add test to make sure check_for_ones doesn't cause an issue

--- tests/testthat/test-mtxDE.R
+++ tests/testthat/test-mtxDE.R
@@ -118,3 +118,62 @@ test_that("run_mtxDE works", {
                  colnames(expected.gamlss))
 
 })
+
+
+test_that("run_mtxDE works linear models", {
+    feature.table <- data.frame(a=c(0.1, 0.0, 0.0, 0.0),
+                                b=c(0.5, 0.5, 0.5, 0.4),
+                                c=c(0.4, 0.5, 0.0, 0.0),
+                                d=c(0.0, 0.0, 0.5, 0.6))
+    row.names(feature.table) <- paste0("sample_", 1:4)
+    metadata <- data.frame(SampleID=paste0("sample_", 1:4),
+                           phenotype=c(0,0,1,1),
+                           participant=c(0,1,0,1),
+                           timepoint=c(0,0,1,1))
+
+    expected.lm <- read.csv("data/expected_mtxDE_results_lm.csv")
+    expected.lmer <- read.csv("data/expected_mtxDE_results_lmer.csv")
+
+    lm.actual <- run_mtxDE("phenotype",
+                           feature.table,
+                           metadata,
+                           reg.method="lm",
+                           sampleID="SampleID")
+
+    expect_equal(lm.actual$p.value,
+                 expected.lm$p.value,
+                 tolerance=1e-3)
+    expect_equal(lm.actual$estimate,
+                 expected.lm$estimate,
+                 tolerance=1 # some of these values are big and variable
+                 # and the p value/CI matters more
+    )
+    expect_equal(colnames(lm.actual),
+                 colnames(expected.lm))
+
+    lmer.actual <- run_mtxDE("phenotype + (1|participant)",
+                            feature.table,
+                            metadata,
+                            reg.method="lmer",
+                            sampleID="SampleID")
+
+    expect_equal(lmer.actual$p.value,
+                 expected.lmer$p.value,
+                 tolerance=1e-3)
+    expect_equal(lmer.actual$estimate,
+                 expected.lmer$estimate,
+                 tolerance=1 # some of these values are big and variable
+                 # and the p value/CI matters more
+    )
+    expect_equal(colnames(lmer.actual),
+                 colnames(expected.lmer))
+
+    # Make sure there aren't errors from values >=1 that don't fit the beta reg
+    feature.table.bigvals <- feature.table*100
+    expect_no_error(run_mtxDE("phenotype + (1|participant)",
+                             feature.table.bigvals,
+                             metadata,
+                             reg.method="lmer",
+                             sampleID="SampleID"))
+})
+
