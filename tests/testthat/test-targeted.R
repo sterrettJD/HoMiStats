2c2e2eed0ad2d8675dc450d2f8e00e62cdc9874b
John Sterrett <sterrettJD@protonmail.com>
2024-04-24 16:07:58 -0600

    add test for kos within a gmm

--- tests/testthat/test-targeted.R
+++ tests/testthat/test-targeted.R
@@ -38,6 +38,8 @@ test_that("we can run DESeq2 for a KO", {
     relevant.host.genes <- c("PIGV","ALG12")
     nonrelevant.host.genes <- c("goofballgene", "sillygoosegene")
     all.host.genes <- c(relevant.host.genes, nonrelevant.host.genes)
+    # certain seeds were causing dispersion issues, so just manually setting that here
+    set.seed(2)
     host.gene.counts <- data.frame(sapply(X=all.host.genes,
                                          FUN=function(x){
                                              rnbinom(n=1000, size=400000, prob=runif(1))
@@ -67,6 +69,55 @@ test_that("we can run DESeq2 for a KO", {
     expect_equal(nrow(results), 2)
     expected.colnames <- c("baseMean", "log2FoldChange", "lfcSE",
                            "stat", "pvalue", "padj")
+    expect_equal(colnames(results), expected.colnames)
+    expect_true(sum(results$padj < 0.05) == 0)
+})
+
+
+test_that("we can run DESeq2 for KOs within a GMM", {
+    GMM.matrix <- suppressMessages(get_GMM_matrix())
+    mtx.feature.names <- c("k02005_hly_d_family_secretion_protein",
+                           "k02007_cobalt_nickel_transport_system_permease_protein",
+                           "k00248_butyrate_gene_1",
+                           "K00634_butyrate_gene_2",
+                           "K00929_butyrate_gene_3")
+
+    # simulate some host data
+    relevant.host.genes <- c("PIGV","ALG12")
+    nonrelevant.host.genes <- c("goofballgene", "sillygoosegene")
+    all.host.genes <- c(relevant.host.genes, nonrelevant.host.genes)
+    set.seed(1)
+    host.gene.counts <- data.frame(sapply(X=all.host.genes,
+                                          FUN=function(x){
+                                              rnbinom(n=1000, size=400000, prob=runif(1))
+                                          }
+    )
+    )
+    # simulate some mtx data
+    microbial.gene <- "k00248_butyrate_gene_1"
+    microbial.gene.counts <- data.frame(sapply(X=mtx.feature.names,
+                                               FUN=function(x){
+                                                   rnorm(n=1000, mean=100)
+                                               }
+    )
+    )
+
+    mtx.features.to.test <- features_from_gmm_df("butyrate production I",
+                                                 GMM.matrix,
+                                                 mtx.feature.names=mtx.feature.names)
+    results <- suppressMessages(
+        suppressWarnings(
+            GO_targeted_for_each_KO_within_GMM(go.terms=c("GO:0000009"),
+                               host.genes=t(host.gene.counts),
+                               mtx=microbial.gene.counts,
+                               mtx.features=mtx.features.to.test)
+        )
+    )
+
+    # There should be 2 rows with no significant results
+    expect_equal(nrow(results), 6)
+    expected.colnames <- c("baseMean", "log2FoldChange", "lfcSE",
+                            "stat", "pvalue", "padj", "term")
     expect_equal(colnames(results), expected.colnames)
     expect_true(sum(results$padj < 0.05) == 0)
 })
