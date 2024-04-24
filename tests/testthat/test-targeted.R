315357cb0bbdc9e91dd0da07e32f100d7a17f7c7
John Sterrett <sterrettJD@protonmail.com>
2024-04-24 10:36:19 -0600

    add gmm kos df

--- tests/testthat/test-targeted.R
+++ tests/testthat/test-targeted.R
@@ -3,5 +3,25 @@ test_that("we can grab gene names from go terms", {
     expected.go.09 <- c("PIGV","ALG12")
     actual.go.09 <- suppressMessages(get_go_term_human_genes("GO:0000009"))
 
-    expect_equal(actual_go_09, expected_go_09)
+    expect_equal(actual.go.09, expected.go.09)
+})
+
+
+test_that("we can grab KOs from GMM matrix", {
+    GMM.matrix <- get_GMM_matrix()
+    mtx.feature.names <- c("k02005_hly_d_family_secretion_protein",
+                           "k02007_cobalt_nickel_transport_system_permease_protein",
+                           "k00248_butyrate_gene_1",
+                           "K00634_butyrate_gene_2",
+                           "K00929_butyrate_gene_3")
+
+    actual.features <- features_from_gmm_df(GMM="butyrate production I",
+                                            GMM.kos.df=GMM.matrix,
+                                            mtx.feature.names=mtx.feature.names)
+    expected.features <- c("k00248_butyrate_gene_1",
+                           "K00634_butyrate_gene_2",
+                           "K00929_butyrate_gene_3")
+
+    expect_equal(actual.features, expected.features)
+
 })
