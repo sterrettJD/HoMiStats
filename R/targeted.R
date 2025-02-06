acbf1eba5cd5a7fe7907cb9083ac1939916a0df8
John Sterrett <sterrettJD@protonmail.com>
2025-02-06 11:26:57 -0700

    add examples and bool write out

--- R/targeted.R
+++ R/targeted.R
@@ -1,12 +1,49 @@
 #' Run differential expression testing on a targeted subset of genes
-#' @description
-#' @param go.terms A string vector with the GO terms of interest
-#' @param host.genes A matrix or data.frame with the host gene count data
-#' @param mtx A data.frame with the normalized microbial gene count data
-#' @param mtx.features A vector of features to use from the microbial gene counts
-#' @param padj A string denoting the p value adustment method. Options can be checked using 'p.adjust.methods'
-#' @return a data.frame with DESeq results for each mtx feature
+#' @description This function runs differential expression analysis on a subset of genes that are associated with specific Gene Ontology (GO) terms.
+#'
+#' @param go.terms A character vector containing the GO terms of interest (e.g., c("GO:0000009", "GO:0042110")).
+#' @param host.genes A matrix or data.frame containing host gene count data, with rows as genes and columns as samples.
+#' @param mtx A data.frame containing normalized microbial gene count data, with rows as microbial genes and columns as samples.
+#' @param mtx.features A character vector specifying the microbial gene features to use.
+#' @param padj A string specifying the p-value adjustment method. Available options can be checked using `p.adjust.methods` (default: "fdr").
+#' @param verbose Logical indicating whether to print progress messages (default: FALSE).
+#'
+#' @return A data.frame with DESeq2 results for each microbial gene feature, including columns for baseMean, log2FoldChange, standard error (lfcSE), statistical significance (stat), p-value, and adjusted p-value (padj).
+#'
 #' @export
+#' @examples
+#' set.seed(123)
+#' GMM.matrix <- suppressMessages(get_GMM_matrix())
+#' mtx.feature.names <- c("k02005_hly_d_family_secretion_protein",
+#'                   "k02007_cobalt_nickel_transport_system_permease_protein",
+#'                   "k00248_butyrate_gene_1",
+#'                   "K00634_butyrate_gene_2",
+#'                   "K00929_butyrate_gene_3")
+#'
+#' # simulate some host data
+#' relevant.host.genes <- c("PIGV","ALG12")
+#' nonrelevant.host.genes <- c("goofballgene", "sillygoosegene")
+#' all.host.genes <- c(relevant.host.genes, nonrelevant.host.genes)
+#' host.gene.counts <- data.frame(sapply(X=all.host.genes,
+#'                                       FUN=function(x){
+#'                                              rnbinom(n=1000,
+#'                                              size=400000, prob=runif(1))})
+#'                                    )
+#'
+#' # simulate some mtx data
+#' microbial.gene.counts <- data.frame(sapply(X=mtx.feature.names,
+#'                                            FUN=function(x){
+#'                                                rnorm(n=1000, mean=100)})
+#'                                     )
+#'
+#' mtx.features.to.test <- features_from_gmm_df("butyrate production I",
+#'                                        GMM.matrix,
+#'                                        mtx.feature.names=mtx.feature.names)
+#'
+#' results <- GO_targeted_for_each_KO_within_GMM(go.terms=c("GO:0000009"),
+#'                               host.genes=t(host.gene.counts),
+#'                               mtx=microbial.gene.counts,
+#'                               mtx.features=mtx.features.to.test)
 #'
 GO_targeted_for_each_KO_within_GMM <- function(go.terms, host.genes,
                                             mtx, mtx.features,
@@ -41,13 +78,20 @@ GO_targeted_for_each_KO_within_GMM <- function(go.terms, host.genes,
     return(results)
 }
 
-#' Pull mtx features for a gut metabolic module of interest
-#' @description Looks for all mtx features corresponding to the gut metabolic module of interest
-#' @param GMM The name of the gut metabolic module of interest
-#' @param GMM.kos.df A data.frame with columns "Module" and "KEGG" with the KOs corresponding to each gut metabolic module. Can be generated using get_GMM_matrix()
-#' @param mtx A vector with all of the metatranscriptome feature names, with KOs in them
-#' @return a vector of feature names corresponding to the GMM of interest
+#' Pull microbial features corresponding to a gut metabolic module (GMM)
+#' @description Extracts all microbial transcriptomic (mtx) features associated with a given gut metabolic module.
+#'
+#' @param GMM A string specifying the gut metabolic module of interest.
+#' @param GMM.kos.df A data.frame with columns "Module" and "KEGG" mapping KOs to their respective gut metabolic modules. Can be generated using `get_GMM_matrix()`.
+#' @param mtx.feature.names A character vector of all metatranscriptomic feature names, including KEGG Orthology (KO) identifiers.
+#'
+#' @return A character vector of microbial features corresponding to the specified GMM.
 #' @export
+#' @examples
+#' GMM.matrix <- suppressMessages(get_GMM_matrix())
+#' features <- features_from_gmm_df(GMM="butyrate production I",
+#'                                  GMM.kos.df=GMM_matrix,
+#'                                  mtx.feature.names=c("K00248", "K00634"))
 #'
 features_from_gmm_df <- function(GMM, GMM.kos.df, mtx.feature.names){
     module.kos <- GMM.kos.df[GMM.kos.df$Module==GMM, "KEGG"]
@@ -62,13 +106,19 @@ features_from_gmm_df <- function(GMM, GMM.kos.df, mtx.feature.names){
 }
 
 
-#' Pull all human genes corresponding to a GO term of interest
-#' @description Looks for all genes corresponding to the GO term of interest
-#' @param go.term A string version of a GO term, such as "GO:0042110"
-#' @return a vector of gene names corresponding to the GO term of interest
+#' Retrieve human genes associated with a GO term
+#' @description Fetches all human genes that are annotated with a given Gene Ontology (GO) term.
+#'
+#' @param go.term A character string representing a GO term, such as "GO:0042110".
+#'
+#' @return A character vector of human gene symbols associated with the GO term.
 #' @export
+#'
 #' @importFrom AnnotationDbi select
 #' @importFrom org.Hs.eg.db org.Hs.eg.db
+#'
+#' @examples
+#' genes <- get_go_term_human_genes("GO:0000009")
 #'
 get_go_term_human_genes <- function(go.term){
     gene.data <- AnnotationDbi::select(org.Hs.eg.db::org.Hs.eg.db,
@@ -82,21 +132,57 @@ get_go_term_human_genes <- function(go.term){
 }
 
 
-#' Run targeted differential expression analysis in DESeq2 for a subset of genes
-#' @description Uses the vector of genes provided to subset the data then run DESeq2
-#' @param targeted.genes A vector of host gene names
-#' @param host.genes A matrix or data.frame with the host gene count data
-#' @param microbial.gene The name of the microbial gene to be used
-#' @param microbial.genes A matrix or data.frame with the microbial gene count data
-#' @return A dataframe of DESeq2 results
+#' Run targeted differential expression analysis using DESeq2
+#' @description Performs differential expression analysis in DESeq2 on a subset of host genes, based on their association with microbial gene expression.
+#'
+#' @param targeted.genes A character vector of host gene names to include in the analysis.
+#' @param host.genes A matrix or data.frame containing host gene count data, with rows as genes and columns as samples.
+#' @param microbial.gene A string specifying the microbial gene of interest.
+#' @param microbial.genes A matrix or data.frame containing microbial gene count data.
+#' @param verbose Logical indicating whether to print progress messages (default: TRUE).
+#'
+#' @return A data.frame with DESeq2 results, including columns for baseMean, log2FoldChange, standard error (lfcSE), statistical significance (stat), p-value, and adjusted p-value (padj).
+#'
 #' @export
 #' @importFrom DESeq2 DESeqDataSetFromMatrix
-#' @importFrom DESeq2 DESeq2
+#' @importFrom DESeq2 DESeq
 #' @importFrom DESeq2 results
+#'
+#' @examples
+#' set.seed(123)
+#' GMM.matrix <- suppressMessages(get_GMM_matrix())
+#' mtx.feature.names <- c("k02005_hly_d_family_secretion_protein",
+#'                   "k02007_cobalt_nickel_transport_system_permease_protein",
+#'                   "k00248_butyrate_gene_1",
+#'                   "K00634_butyrate_gene_2",
+#'                   "K00929_butyrate_gene_3")
+#'
+#' # simulate some host data
+#' relevant.host.genes <- c("PIGV","ALG12")
+#' nonrelevant.host.genes <- c("goofballgene", "sillygoosegene")
+#' all.host.genes <- c(relevant.host.genes, nonrelevant.host.genes)
+#' host.gene.counts <- data.frame(sapply(X=all.host.genes,
+#'                                       FUN=function(x){
+#'                                              rnbinom(n=1000,
+#'                                              size=400000, prob=runif(1))})
+#'                                    )
+#'
+#' # simulate some mtx data
+#' microbial.gene <- "k00248_butyrate_gene_1"
+#' microbial.gene.counts <- data.frame(sapply(X=mtx.feature.names,
+#'                                            FUN=function(x){
+#'                                                rnorm(n=1000, mean=100)})
+#'                                     )
+#'
+#' results <- go_targeted_diffex(targeted.genes=relevant.host.genes,
+#'                               host.genes=t(host.gene.counts),
+#'                               microbial.gene=microbial.gene,
+#'                               microbial.genes=microbial.gene.counts,
+#'                               verbose=FALSE)
 #'
 go_targeted_diffex <- function(targeted.genes, host.genes,
                                microbial.gene, microbial.genes,
-                               verbose=T){
+                               verbose=TRUE){
     targeted.data <- host.genes[targeted.genes,]
     if(verbose){
         s1 <- paste0(length(targeted.genes), " genes identified from GO term")
